657cef125209fce94c45c4a4c55f6405d48223cb
@Vika@ <frantsuhvika@gmail.com>
2020-01-07 01:32:50 +0200

    Start to implementing NavigationView(DrawerLayout)

--- app/src/main/java/ua/at/fvi/remindme/MainActivity.java
+++ app/src/main/java/ua/at/fvi/remindme/MainActivity.java
@@ -3,15 +3,18 @@ package ua.at.fvi.remindme;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
 private Toolbar toolbar;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //default theme settings in styles.xml
+        setTheme(R.style.AppDefault);
         setContentView(R.layout.activity_main);
         initToolbar();
     }
